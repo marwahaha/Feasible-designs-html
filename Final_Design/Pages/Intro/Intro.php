06a3da0440405075b0cae6444d88f197d459b3ad
Gaysha1337 <33755321+Gaysha1337@users.noreply.github.com>
2018-04-11 10:44:56 +0200

    Update Intro.php

--- Final_Design/Pages/Intro/Intro.php
+++ Final_Design/Pages/Intro/Intro.php
@@ -8,6 +8,7 @@ $conn = mysqli_connect($server, $user, $pass, $db);
 $sql = "INSERT INTO `users` (`user_names`) VALUES ($person)";
 
 //error_log("txt== ".$person);
+// Reset auto increment ALTER TABLE `users` AUTO_INCREMENT=1;
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
