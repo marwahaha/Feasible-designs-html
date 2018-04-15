304df842c25ceb3ca6088ede02c30ebf691c0920
Gaysha1337 <33755321+Gaysha1337@users.noreply.github.com>
2018-04-15 20:46:59 +0200

    Add files via upload

---
+++ Final_Design/Pages/Intro/Intro.php
@@ -0,0 +1,44 @@
+﻿<?php
+$server = "localhost";
+$user = 'root';
+$pass = '';
+$db = "player_names";
+$person = $_POST['txt'];
+$conn = mysqli_connect($server, $user, $pass, $db);
+$sql = "INSERT INTO `users` (`user_names`) VALUES ($person)";
+
+//error_log("txt== ".$person);
+// Reset auto increment ALTER TABLE `users` AUTO_INCREMENT=1;
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$sql = "INSERT INTO `users` (`user_names`) VALUES ('$person')";
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
+<!doctype html>
+<html>
+<head>
+	<title> Example</title>
+</head>
+<body>
+<ol>
+   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
+   <li>Aliquam tincidunt mauris eu risus.</li>
+   <li>Vestibulum auctor dapibus neque.</li>
+</ol>
+
+<p>
+This page is empty, here would go one of my scenarios
+
+THIIICCC
+</p>
+</body>
+</html>
---
+++ Final_Design_Copy/Pages/Intro/Intro.php
@@ -0,0 +1,28 @@
+﻿
+<?php
+$_SERVER['REQUEST_METHOD'] === 'POST';
+$server = "ptvp.myd.infomaniak.com";
+$user = 'ptvp_dmitriy';
+$pass = 'nations2018';
+$db = "ptvp_player_names";
+$person = $_POST['txt'];
+$conn = mysqli_connect($server, $user, $pass, $db);
+$sql = "INSERT INTO `users` (`user_names`) VALUES ('$person')";
+
+error_log("txt== ".$person);
+// Reset auto increment ALTER TABLE `users` AUTO_INCREMENT=1;
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+if (mysqli_query($conn, $sql)) {
+      echo "New record created successfully";
+	  echo error_log("txt== ".$person);
+} else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+
